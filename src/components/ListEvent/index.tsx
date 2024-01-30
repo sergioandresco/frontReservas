3fa9d51adafe2cef348ee4f42ddd57e34a31d5a7
sergioandresco <programador2@interactiva.net.co>
2024-01-30 16:41:25 -0500

    Validation of available seats is created with the seats that the user wants to reserve

--- src/components/ListEvent/index.tsx
+++ src/components/ListEvent/index.tsx
@@ -5,11 +5,14 @@ function ListEvent() {
     const [events, setEvents] = useState([]);
     const [showReserv, setShowReserv] = useState(false);
     const [selectedEventId, setSelectedEventId] = useState(null);
+    const [selectedEventPlaces, setSelectedEventPlaces] = useState(null);
 
-    const handleClick = (eventId) => {
+
+    const handleClick = (eventId, eventPlaces) => {
         setSelectedEventId(eventId);
+        setSelectedEventPlaces(eventPlaces);
         setShowReserv(true);
-    }; 
+    };
 
     useEffect(() => {
         fetch('http://localhost:8000/all-events/')
@@ -20,14 +23,14 @@ function ListEvent() {
 
     return (
         <div>
-            {showReserv ? <Reserv selectedEventId={selectedEventId} /> : events.map((event, index) => (
+            {showReserv ? <Reserv selectedEventId={selectedEventId} selectedEventPlaces={selectedEventPlaces} /> : events.map((event, index) => (
                 <div key={index}>
                     <h2>{event.name}</h2>
                     <p>{event.description}</p>
                     <p>{event.date_event}</p>
                     <p>{event.number_of_places}</p>
                     <p>{event.number_of_places_available}</p>
-                    <button onClick={() => handleClick(event.id)}>Ingresar</button>
+                    <button onClick={() => handleClick(event.id, event.number_of_places_available)}>Ingresar</button>
                 </div>
             ))}
         </div>
--- src/components/Reserv/index.tsx
+++ src/components/Reserv/index.tsx
@@ -1,13 +1,42 @@
 import React, { useState } from "react";
 
-function Reserv({ selectedEventId }){
+function Reserv({ selectedEventId, selectedEventPlaces }){
 
     const [quantity, setQuantity] = useState(0);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
 
     console.log(selectedEventId)
 
+    // const handleSubmit = async (event) => {
+    //     event.preventDefault();
+    
+    //     const response = await fetch('http://localhost:8000/create-reserv/', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify({
+    //             places_reserv: quantity,
+    //             event: selectedEventId,
+    //             user: 1,
+    //         }),
+    //     });
+    
+    //     if (!response.ok) {
+    //         console.error('Error:', response.statusText);
+    //     } else {
+    //         console.log('Reserva realizada con éxito');
+    //     }
+    // };    
+
     const handleSubmit = async (event) => {
         event.preventDefault();
+    
+        if (quantity > selectedEventPlaces) {
+            setErrorMessage('No hay suficientes lugares disponibles');
+            return;
+        }
     
         const response = await fetch('http://localhost:8000/create-reserv/', {
             method: 'POST',
@@ -24,15 +53,18 @@ function Reserv({ selectedEventId }){
         if (!response.ok) {
             console.error('Error:', response.statusText);
         } else {
-            console.log('Reserva realizada con éxito');
+            setSuccessMessage('Reserva realizada con éxito');
         }
     };    
 
     return(
         <form onSubmit={handleSubmit}>
             <h1>{selectedEventId}</h1>
+            <h1>{selectedEventPlaces}</h1>
             <label htmlFor="">Cantidad de lugares a reservar: </label>
             <input type="number" value={quantity} onChange={(e) => setQuantity(Number(e.target.value))} />
+            {errorMessage && <p>{errorMessage}</p>}
+            {successMessage && <p>{successMessage}</p>}
             <button>Reservar</button>
         </form>
     )
